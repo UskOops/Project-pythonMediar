47657e3f5719284825f6a65e990004384e1e60a2
Marco A. L. Dias <78981847+UskOops@users.noreply.github.com>
2021-12-01 13:56:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -16,12 +16,12 @@ blobs = list(storage_client.list_blobs(bucket, prefix="stura")) #list client and
 for blob in blobs:
     if blob.name.endswith(".json"): #search all archive ends with ".json"
         print(blob.name)
-        filename = os.path.join("/temp",blob.name.replace("/","%%")) 
+        filename = os.path.join("/temp",blob.name.replace("/","%%")) #tell the code to make a download e repleace some caracteres
         blob.download_to_filename(filename)
         with open(filename) as yuri: #yuri is a exemple name
             json_data = json.load(yuri)
             for roi in json_data['shapes']:
                 if roi['attributes'][0]['value'] != 'crop':
-                    roi_id = int(roi['attributes'][0]['value']) + 10**9
-                    print(f'cam: {roi["frame"]} - roi: {roi_id}')
+                    roi_id = int(roi['attributes'][0]['value']) + 10**9 #command to make find all archives having attributes, values 0 
+                    print(f'cam: {roi["frame"]} - roi: {roi_id}') #local where we can find this archives
 
